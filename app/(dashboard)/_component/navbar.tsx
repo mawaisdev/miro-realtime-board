913e2db2acbbcc554fe47e9663db33a57ac2213f
Muhammad Awais <m.awais2030@gmail.com>
2024-02-12 17:49:50 +0500

    Navbar Complete

--- app/(dashboard)/_component/navbar.tsx
+++ app/(dashboard)/_component/navbar.tsx
@@ -1,14 +1,46 @@
 'use client'
 
-import { UserButton } from '@clerk/nextjs'
+import {
+  OrganizationSwitcher,
+  UserButton,
+  useOrganization,
+} from '@clerk/nextjs'
+
+import { SearchInput } from '@/app/(dashboard)/_component/search-input'
+import { InviteButton } from '@/app/(dashboard)/_component/invite-button'
 
 export const Navbar = () => {
+  const { organization } = useOrganization()
   return (
-    <div className='flex items-center gap-x-4 p-5 bg-green-400'>
-      <div className='hidden lg:flex lg:flex-1 bg-slate-500'>
-        {/* Add Search bar here */}
-        Search
+    <div className='flex items-center gap-x-4 p-5'>
+      <div className='hidden lg:flex lg:flex-1'>
+        <SearchInput />
       </div>
+      <div className='block lg:hidden flex-1'>
+        <OrganizationSwitcher
+          hidePersonal
+          appearance={{
+            elements: {
+              rootBox: {
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                width: '100%',
+                maxWidth: '376px',
+              },
+              organizationSwitcherTrigger: {
+                padding: '6px',
+                width: '100%',
+                borderRadius: '8px',
+                border: '1px solid #E5E7EB',
+                justifyContent: 'space-between',
+                backgroundColor: 'white',
+              },
+            },
+          }}
+        />
+      </div>
+      {organization && <InviteButton />}
       <UserButton />
     </div>
   )
