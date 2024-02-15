a0231dbbacf839ce3ddac40ceec94b22b4b5a492
Muhammad Awais <m.awais2030@gmail.com>
2024-02-15 12:04:34 +0500

    Refactored

--- app/board/[boardId]/_components/participants.tsx
+++ app/board/[boardId]/_components/participants.tsx
@@ -7,8 +7,7 @@ export const Participants = () => {
     </div>
   )
 }
-
-Participants.Skeleton = function ParticipantsSkeleton() {
+export const ParticipantsSkeleton = () => {
   return (
     <div className='absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md w-[100px]' />
   )
