7216804e4e9a6f2bf169a0f50a59e6c81bdecf2d
Muhammad Awais <m.awais2030@gmail.com>
2024-02-16 16:31:58 +0500

    Canvas State for Selecting Tools from Toolbar

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,19 +1,39 @@
 'use client'
 
+import { useState } from 'react'
+
 import { Info } from '@/app/board/[boardId]/_components/info'
 import { Toolbar } from '@/app/board/[boardId]/_components/toolbar'
 import { Participants } from '@/app/board/[boardId]/_components/participants'
+
+import { useHistory, useCanRedo, useCanUndo } from '@/liveblocks.config'
+import { CanvasMode, CanvasState } from '@/types/canvas'
 
 interface CanvasProps {
   boardId: string
 }
 
 export const Canvas = ({ boardId }: CanvasProps) => {
+  const [canvasState, setCanvasState] = useState<CanvasState>({
+    mode: CanvasMode.None,
+  })
+
+  const { undo, redo } = useHistory()
+  const canUndo = useCanUndo()
+  const canRedo = useCanRedo()
+
   return (
     <main className='h-full w-full relative bg-neutral-100 touch-none'>
       <Info boardId={boardId} />
       <Participants />
-      <Toolbar />
+      <Toolbar
+        canvasState={canvasState}
+        setCanvasState={setCanvasState}
+        canRedo={canRedo}
+        canUndo={canUndo}
+        undo={undo}
+        redo={redo}
+      />
     </main>
   )
 }
