b83a3ba86932f7770c4ef5af043f9bbe53ecb2f5
Muhammad Awais <m.awais2030@gmail.com>
2024-02-21 12:27:30 +0500

    Utility func to Resize a Layer

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,7 @@
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
-import { Camera, Color } from '@/types/canvas'
+import { Camera, Color, Point, Side, XYWH } from '@/types/canvas'
 
 const COLORS = [
   '#FFC0CB',
@@ -67,4 +67,34 @@ export function colorToCSS({ r, g, b }: Color) {
   return `#${r.toString(16).padStart(2, '0')}${g
     .toString(16)
     .padStart(2, '0')}${b.toString(16).padStart(2, '0')}`
+}
+
+export function resizeBounds(bounds: XYWH, corner: Side, point: Point): XYWH {
+  const result = {
+    x: bounds.x,
+    y: bounds.y,
+    width: bounds.width,
+    height: bounds.height,
+  }
+
+  if ((corner & Side.Left) === Side.Left) {
+    result.x = Math.min(point.x, bounds.x + bounds.width)
+    result.width = Math.abs(bounds.x + bounds.width - point.x)
+  }
+
+  if ((corner & Side.Right) === Side.Right) {
+    result.x = Math.min(point.x, bounds.x)
+    result.width = Math.abs(point.x - bounds.x)
+  }
+
+  if ((corner & Side.Top) === Side.Top) {
+    result.y = Math.min(point.y, bounds.y + bounds.height)
+    result.height = Math.abs(bounds.y + bounds.height - point.y)
+  }
+
+  if ((corner & Side.Bottom) === Side.Bottom) {
+    result.y = Math.min(point.y, bounds.y)
+    result.height = Math.abs(point.y - bounds.y)
+  }
+  return result
 }
